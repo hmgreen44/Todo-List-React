cafcfa8d6d3209c1ce0cca251a92dff3ae799588
hmgreen44 <68399145+hmgreen44@users.noreply.github.com>
2021-03-22 13:53:51 -0400

    setup project for code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import './App.css';
+import Todos from "./Components/Todos"
 
 function App() {
   return (
     <div className="App">
-    <h1>My Todo List</h1>
+      <Todos />
     </div>
   );
 }
